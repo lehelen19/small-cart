3fea686f8ec648271d6bdf28d495b94dc5a6cc03
Helen Le <le.helen19@gmail.com>
2022-07-26 15:06:28 -0700

    Add remove from cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -12,18 +13,29 @@ function App() {
   const onAdd = (product) => {
     const doesExist = cartItems.find((x) => x.id === product.id);
     if (doesExist) {
-      // eslint-disable-next-line max-len
       setCartItems(cartItems.map((x) => (x.id === product.id ? { ...doesExist, qty: doesExist.qty + 1 } : x)));
     } else {
       setCartItems([...cartItems, { ...product, qty: 1 }]);
     }
   };
+
+  const onRemove = (product) => {
+    const doesExist = cartItems.find((x) => x.id === product.id);
+    if (doesExist.qty === 1) {
+      // Remove product from list
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      // Decrease quantity
+      setCartItems(cartItems.map((x) => (x.id === product.id ? { ...doesExist, qty: doesExist.qty - 1 } : x)));
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="row">
         <Main onAdd={onAdd} products={products} />
-        <Cart onAdd={onAdd} cartItems={cartItems} />
+        <Cart onAdd={onAdd} onRemove={onRemove} cartItems={cartItems} />
       </div>
     </div>
   );
