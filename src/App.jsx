fd22c11d3620a78a5f39af04dc455c1ec82339e0
Helen Le <le.helen19@gmail.com>
2022-07-25 19:45:35 -0700

    Create basic components: Main, Header, Cart (all empty)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css';
 import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <Header />
+      <div>
+        <Main />
+        <Cart />
+      </div>
     </div>
   );
 }
